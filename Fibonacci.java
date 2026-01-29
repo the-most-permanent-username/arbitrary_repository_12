cc815fcc9f0aaa950cac6780bce3a42dfb936b7a
Mac-S-13 <macschwieterman@gmail.com>
2026-01-29 15:30:27 -0500

    Wassup bro added n == 2 case

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,13 +6,13 @@ public class Fibonacci {
 	public long getFibTerm(int n) {
 		if (n < 0) {
 			throw new IllegalArgumentException(n + " is negative");
-		}
-		else if (n == 0) {
+		}else if (n == 0) {
 			return 0;
-		}
-			else if (n == 1) {
+		}else if (n == 1) {
 			return 1;
-		} else {
+		}else if(n == 2){
+			return getFibTerm(n - 1) + getFibTerm(n - 2);
+		}else {
 			if(n < 100) {
 				if(fib_array[n] != 0) {
 					return fib_array[n];
